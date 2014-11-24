a106d684970839a1eb42533739b9cdaa8052290f
jacob <jtk217@uw.edu>
2014-11-24 12:12:55 -0800

    Changes made in class

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,17 @@
 
 'use strict';
 
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/foo', function(req, res) {
+    res.send('you requested foo!');
+});
+
+app.listen(8080, function() {
+    console.log('Server is listening at http://localhost:8080');
+});
